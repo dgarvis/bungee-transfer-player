2d7ee01b24be558a30e8584eef73089649c50693
Dylan Garvis <dylan@garvis.dev>
2024-01-13 10:27:23 -0500

    Worknig on basic shell, does not load yet.

---
+++ src/main/java/dev/garvis/bungeetransfer/BungeeTransferPlugin.java
@@ -0,0 +1,53 @@
+package dev.garvis.bungeetransfer;
+
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.File;
+
+public class BungeeTransferPlugin extends Plugin {
+
+    
+    @Override
+    public void onEnable() {
+	try {
+	    makeConfig();
+	} catch (IOException e) {
+	    getLogger().warning("Could not create default config.");
+	}
+
+	Configuration config;
+	try {
+	    config = ConfigurationProvider.getProvider(YamlConfiguration.class)
+		.load(new File(getDataFolder(), "config.yml"));
+	} catch (IOException e) {
+	    getLogger().warning("Could not load config.");
+	    return;
+	}
+
+	// TODO
+	
+	getLogger().info("onEnable");
+    }
+
+    public void makeConfig() throws IOException {
+	// Create plugin config folder if it doesn't exist
+	if (!getDataFolder().exists()) {
+	    getLogger().info("Created config folder: " + getDataFolder().mkdir());
+	}
+	
+	File configFile = new File(getDataFolder(), "config.yml");
+
+	// Copy default config if it doesn't exist
+	if (!configFile.exists()) {
+	    FileOutputStream outputStream = new FileOutputStream(configFile); // Throws IOException
+	    InputStream in = getResourceAsStream("config.yml"); // This file must exist in the jar resources folder
+	    in.transferTo(outputStream); // Throws IOException
+	}
+    }
+}
